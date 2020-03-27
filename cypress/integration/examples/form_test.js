76c8a03281bc95ffcf7b5db3d008876e6bf2ea02
miriam-sullivan <maljayd42@gmail.com>
2020-03-27 15:08:22 -0500

    added all the tests

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -9,5 +9,14 @@ describe("testing form inputs", function() {
     cy.get('input[name="email"]')
       .type("email1@email.com")
       .should("have.value", "email1@email.com");
+    cy.get('input[name="password"]')
+      .type("cats12345")
+      .should("have.value", "cats12345");
+    cy.get('[type="checkbox"]').check();
+    cy.get("button").click();
+    cy.wait(2000);
+    cy.get('input[name="name"]')
+      .type("myco")
+      .clear();
   });
 });
